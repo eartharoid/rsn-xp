c0fa61651d10275a6a4a74ff7198460ee99947c7
Isaac <git@eartharoid.me>
2022-01-24 14:23:16 +0000

    fix max level

--- src/functions.js
+++ src/functions.js
@@ -1,2 +1,2 @@
-module.exports.calcLevel = points => Math.floor(0.1 * Math.sqrt(points));
+module.exports.calcLevel = points => Math.min(Math.floor(0.1 * Math.sqrt(points)), 10);
 module.exports.formatTime = mins => mins > 60 ? `${Math.floor(mins / 60)}h ${mins % 60}m` : `${mins}m`;
