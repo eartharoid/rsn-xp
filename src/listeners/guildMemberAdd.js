294231dfd213c2aefc63bea018a71bafa4f6f3cd
Isaac <git@eartharoid.me>
2022-01-28 14:25:34 +0000

    don't give level 0 role

--- src/listeners/guildMemberAdd.js
+++ src/listeners/guildMemberAdd.js
@@ -8,7 +8,9 @@ module.exports = async (client, member) => {
 	client.log.info(`"${member.user.tag}" joined "${member.guild.name}"`);
 	let row = await client.prisma.user.findUnique({ where: { id: member.user.id } });
 	if (!row) row = await client.prisma.user.create({ data: { id: member.user.id } });
-	let role = member.guild.roles.cache.find(r => r.name.toLowerCase() === `level ${row.level}`);
-	if (!role) role = await member.guild.roles.create({ name: `Level ${row.level}` });
-	await member.roles.add(role);
+	if (row.level >= 1) {
+		let role = member.guild.roles.cache.find(r => r.name.toLowerCase() === `level ${row.level}`);
+		if (!role) role = await member.guild.roles.create({ name: `Level ${row.level}` });
+		await member.roles.add(role);
+	}
 };
